d0e559abe9b5a56ec5f8ac1e6ad7f86d5b8faf4b
WangQianao <53828244+WangQianao@users.noreply.github.com>
2020-09-11 21:37:10 -0500

    Add files via upload
    在猪类中新增两个数据成员和成员函数

--- C++PigGame/Pig.h
+++ C++PigGame/Pig.h
@@ -12,6 +12,8 @@ class Pig
     PigBreed::Type breed;
     double weight;
     int growDay;
+    bool infected=false;
+    int infectedDay=0;
 public:
     static constexpr double weightMax=75;
     static constexpr int salesPrice[3]={30,14,12};
@@ -19,6 +21,9 @@ public:
     Pig* succ;
     Pig();
     Pig(PigBreed::Type breed);
+    bool isInfected(){return infected;}
+    int getInfectedDay(){return infectedDay;}
+
     double getWeight(){return weight;}
     void setWeight(double a){ weight=a;}
     PigBreed::Type getBreed(){return breed;}
